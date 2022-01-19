6e86bc1b0e58158b78ca53cb969d174a04b4f52a
Jerry Overton <jerryaoverton@yahoo.com>
2022-01-19 13:59:34 -0600

    refactoring the ai

--- highcliff/ai/ai.py
+++ highcliff/ai/ai.py
@@ -1,7 +1,5 @@
 # copying the state of the world for reflection
 import copy
-# the goals the ai is to pursue
-import goals
 
 from highcliff.actions.actions import ActionStatus
 
@@ -9,84 +7,90 @@ from highcliff.actions.actions import ActionStatus
 from goap.planner import RegressivePlanner
 
 
-# these are the things charlie is capable of doing
-capabilities = []
+class AI:
+    # these are the things the Highcliff AI is capable of doing
+    __the_world_GLOBAL_VARIABLE = {}
+    __capabilities_GLOBAL_VARIABLE = []
+    __goals = {}
+    __diary = []
 
-# set the current state of the world
-# a global variable is used to simulate a central message queue
-the_world_GLOBAL_VARIABLE = {}
+    def __init__(self, the_world_global_variable, capabilities_global_variable, goals, life_span_in_iterations):
+        # set the state of the world
+        # a global variable is used to simulate a central message queue
+        self.__the_world_GLOBAL_VARIABLE = the_world_global_variable
 
+        # set the available capabilities
+        # a global variable is used to simulate a central message queue
+        self.__capabilities_GLOBAL_VARIABLE = capabilities_global_variable
 
-# this function returns the current state of the world
-# here is where we put code to read from the (AWS) infrastructure
-def get_world_state():
-    return the_world_GLOBAL_VARIABLE
+        self.__goals = goals
 
+        for iteration in range(life_span_in_iterations):
+            self.__run_ai()
 
-def select_goal(prioritized_goals):
-    # the default is to select an empty goal
-    selected_goal = {}
+    def __get_world_state(self):
+        # this function returns the current state of the world
+        # here is where we put code to read from the (AWS) infrastructure
+        return self.__the_world_GLOBAL_VARIABLE
 
-    # go through goals in priority order
-    for goal in prioritized_goals:
-        # find a goal that is not realized in the world
-        the_goal_is_not_met = prioritized_goals[goal] != get_world_state()[goal]
-        if the_goal_is_not_met:
-            # select the first (highest priority) unrealized goal
-            selected_goal = {goal: prioritized_goals[goal]}
-            break
+    def __select_goal(self, prioritized_goals):
+        # the default is to select an empty goal
+        selected_goal = {}
 
-    return selected_goal
+        # go through goals in priority order
+        for goal in self.__goals:
+            # find a goal that is not realized in the world
+            the_goal_is_not_met = self.__goals[goal] != self.__get_world_state()[goal]
+            if the_goal_is_not_met:
+                # select the first (highest priority) unrealized goal
+                selected_goal = {goal: self.__goals[goal]}
+                break
 
+        return selected_goal
 
-def perceive():
-    goal = select_goal(goals)
-    world_state_snapshot = copy.copy(get_world_state())
-    return goal, world_state_snapshot
+    def __perceive(self):
+        goal = self.__select_goal(self.__goals)
+        world_state_snapshot = copy.copy(self.__get_world_state())
+        return goal, world_state_snapshot
 
+    def __reason(self, goal, world_state):
+        planner = RegressivePlanner(world_state, self.__capabilities_GLOBAL_VARIABLE)
+        plan = planner.find_plan(goal)
+        return plan
 
-def reason(goal, world_state):
-    planner = RegressivePlanner(world_state, capabilities)
-    plan = planner.find_plan(goal)
-    return plan
+    def __act(self, plan):
+        # we are optimistic and assume the plan will succeed
+        action_status = ActionStatus.SUCCESS
 
+        # walk through the plan and execute it
+        for action in plan:
+            action.action.act()
+            # compare intended and actual effects, break if the plan is not working
+            plan_is_not_working = action.action.effects != action.action.actual_effects
+            if plan_is_not_working:
+                action_status = ActionStatus.FAIL
+                break
 
-def act(plan):
-    # we are optimistic and assume the plan will succeed
-    action_status = ActionStatus.SUCCESS
+        world_state_after = self.__get_world_state()
+        return action_status, world_state_after
 
-    # walk through the plan and execute it
-    for action in plan:
-        action.action.act()
-        # compare intended and actual effects, break if the plan is not working
-        plan_is_not_working = action.action.effects != action.action.actual_effects
-        if plan_is_not_working:
-            action_status = ActionStatus.FAIL
-            break
+    def __reflect(self, goal, world_state_before, plan, action_status, world_state_after):
+        diary_entry = {
+            "my_goal": goal,
+            "the_world_state_before": world_state_before,
+            "my_plan": plan,
+            "action_status": action_status,
+            "the_world_state_after": world_state_after
+        }
+        self.__diary.append(diary_entry)
 
-    world_state_after = get_world_state()
-    return action_status, world_state_after
+    def __run_ai(self):
+        # low-level brain function: perception
+        goal, world_state_before = self.__perceive()
 
+        # middle-level brain function: reasoning and planning
+        plan = self.__reason(goal, world_state_before)
 
-def reflect(goal, world_state_before, plan, action_status, world_state_after):
-    diary_entry = {
-        "my_goal": goal,
-        "the_world_state_before": world_state_before,
-        "my_plan": plan,
-        "action_status": action_status,
-        "the_world_state_after": world_state_after
-    }
-    return diary_entry
-
-
-def run_charlie(diary):
-    # low-level brain function: perception
-    goal, world_state_before = perceive()
-
-    # middle-level brain function: reasoning and planning
-    plan = reason(goal, world_state_before)
-
-    # high-level brain function: action and introspection
-    action_status, world_state_after = act(plan)
-    diary_entry = reflect(goal, world_state_before, plan, action_status, world_state_after)
-    diary.append(diary_entry)
+        # high-level brain function: action and introspection
+        action_status, world_state_after = self.__act(plan)
+        self.__reflect(goal, world_state_before, plan, action_status, world_state_after)
