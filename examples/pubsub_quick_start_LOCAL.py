e83179ec76f21a9b99afe11fd3ff2532a0823e69
Jerry Overton <jerryaoverton@yahoo.com>
2022-02-23 10:22:45 -0600

    rewrote the pub sub example

--- examples/pubsub_quick_start_LOCAL.py
+++ examples/pubsub_quick_start_LOCAL.py
@@ -1,15 +1,11 @@
-# needed to access the publish/subscribe functionality of Highcliff
-from highcliff.pubsub import create_topic, publish, subscribe
+# needed to run and access the infrastructure needed to communicate and coordinate
+from highcliff.infrastructure import LocalNetwork, InvalidTopic
 
-# define the message queue global variable
-# when running a local version of Highcliff, use global variables to simulate the underlying MQ infrastructure
-# these global variables will be replaced with a url in the production version
-queue_GLOBAL_VARIABLE = {}
-the_world_GLOBAL_VARIABLE = {}
+# define the infrastructure that provides the message queue functionality
+network = LocalNetwork.instance()
 
 # define the topic to which we will publish/subscribe
 topic_something_on_my_ear = "0020"
-create_topic(queue_GLOBAL_VARIABLE, topic_something_on_my_ear)
 
 
 # this is the function that will be run when a message is published to our topic
@@ -18,12 +14,13 @@ def callback(topic, message):
     print("we received this message: ", message)
 
 
-# subscribe to the topic
-subscribe(queue_GLOBAL_VARIABLE, topic_something_on_my_ear, callback)
+# create and subscribe to the topic
+network.create_topic(topic_something_on_my_ear)
+network.subscribe(topic_something_on_my_ear, callback)
 
 # publish a message to the topic
-message = {
+test_message = {
     "payload": "this is a test message",
-    "effects": [{"first_effect": True}, {"second_effect": True}]
+    "effects": {"first_effect": True, "second_effect": True}
 }
-publish(queue_GLOBAL_VARIABLE, the_world_GLOBAL_VARIABLE, topic_something_on_my_ear, message)
+network.publish(topic_something_on_my_ear, test_message)
