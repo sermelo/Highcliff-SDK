057afb5315441954e6af0c2a094e6d0c8986beb4
Jerry Overton <jerryaoverton@yahoo.com>
2022-02-25 18:26:26 -0600

    successfully tested ai shared reference

--- highcliff/ai/test_ai.py
+++ highcliff/ai/test_ai.py
@@ -4,6 +4,7 @@ import unittest
 from highcliff.infrastructure import LocalNetwork
 from highcliff.exampleactions import MonitorBodyTemperature
 from highcliff.ai import AI
+from highcliff.actions import ActionStatus
 
 
 class TestAI(unittest.TestCase):
@@ -15,6 +16,46 @@ class TestAI(unittest.TestCase):
     def tearDown(self):
         # reset the ai
         self.highcliff.reset()
+
+    def test_shared_ai_reference(self):
+        # create two instances of the ai
+        first_ai = AI.instance()
+        second_ai = AI.instance()
+
+        # the two instances should be the same
+        self.assertTrue(first_ai is second_ai)
+
+        network_of_first_ai = first_ai.network()
+
+        # define a test body temperature monitor with a blank custom behavior
+        class TestAction(MonitorBodyTemperature):
+            def behavior(self):
+                pass
+
+        # instantiate the test body temperature monitor
+        TestAction(network_of_first_ai)
+
+        # define the test world state and goals
+        network_of_first_ai.update_the_world({})
+        first_ai.set_goals({"is_room_temperature_change_needed": True})
+
+        # run a local version of Highcliff
+        first_ai.run(life_span_in_iterations=1)
+
+        # the ai diary should have content
+        self.assertEqual(ActionStatus.SUCCESS, first_ai.diary()[0]['action_status'])
+
+        # the two instances should still be the same after running one
+        self.assertEqual(first_ai.diary(), second_ai.diary())
+
+        # reset the ai
+        first_ai.reset()
+
+        # the content of the ai diary should be gone
+        self.assertEqual([], first_ai.diary())
+
+        # the two instances should still be the same after resting one
+        self.assertTrue(first_ai is second_ai)
 
     def test_goal_not_in_any_actions(self):
         # if the ai has no registered actions to achieve a goal, it should end with no plan
