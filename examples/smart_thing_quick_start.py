51606c4504655b81cac8b062b52310f92b382fb0
Jerry Overton <jerryaoverton@yahoo.com>
2022-02-27 21:17:41 -0600

    Update smart_thing_quick_start.py

--- examples/smart_thing_quick_start.py
+++ examples/smart_thing_quick_start.py
@@ -1,17 +1,15 @@
 # needed to run a local version of the AI
 from highcliff.ai import AI
 
-from highcliff.actions import ActionStatus
-
 # the Highcliff actions to be tested
-from highcliff.exampleactions import MonitorBodyTemperature, AuthorizeRoomTemperatureChange, ChangeRoomTemperature
+from highcliff.exampleactions import MonitorBodyTemperature
 
 # get a reference to the ai and its network
 highcliff = AI.instance()
 network = highcliff.network()
 
 
-# execute a single action with a single goal
+# execute a single action with a single goal:
 
 # define a test body temperature monitor
 class TestBodyTemperatureMonitor(MonitorBodyTemperature):
