5b5cca7d24beae3a192e8c4381dd46cbaa117759
Sergio Merino Alonso <sergio.merinoalonso@dxc.com>
2022-03-18 14:09:16 +0000

    Improve MQTT management

--- aws/iot/python/things.py
+++ aws/iot/python/things.py
@@ -5,7 +5,6 @@ import time
 
 from awscrt import io
 from awsiot import mqtt_connection_builder
-import boto3
 
 
 class Thing():
@@ -24,11 +23,44 @@ class Thing():
         self.mqtt_client = mqtt_connection_builder.mtls_from_path(
             client_id=self.device_id,
             endpoint=endpoint,
-            cert_filepath=cert_filepath,
             client_bootstrap=client_bootstrap,
+            cert_filepath=cert_filepath,
             pri_key_filepath=pri_key_filepath,
+            on_connection_interrupted=self.on_connection_interrupted,
+            on_connection_resumed=self.on_connection_resumed,
             clean_session=False,
-            keep_alive_secs=30)
+            keep_alive_secs=30,
+        )
+        print(f'Connecting to {endpoint} with client ID {self.device_id}')
+        connect_future = self.mqtt_client.connect()
+        # Future.result() waits until a result is available
+        connect_future.result()
+        print('Connected')
+
+    # Callback when connection is accidentally lost.
+    def on_connection_interrupted(self, connection, error, **kwargs):
+        print("Connection interrupted. error: {}".format(error))
+
+
+    # Callback when an interrupted connection is re-established.
+    def on_connection_resumed(self, connection, return_code, session_present, **kwargs):
+        print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+
+        if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+            print("Session did not persist. Resubscribing to existing topics...")
+            resubscribe_future, _ = connection.resubscribe_existing_topics()
+
+            # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+            # evaluate result with a callback instead.
+            resubscribe_future.add_done_callback(self.on_resubscribe_complete)
+
+    def on_resubscribe_complete(self, resubscribe_future):
+        resubscribe_results = resubscribe_future.result()
+        print("Resubscribe results: {}".format(resubscribe_results))
+
+        for topic, qos in resubscribe_results['topics']:
+            if qos is None:
+                sys.exit("Server rejected resubscribe to topic: {}".format(topic))
 
     def run(self):
         while True:
@@ -36,17 +68,21 @@ class Thing():
             time.sleep(self.publish_delay)
 
     def publish_data(self):
-        payload = json.dumps({
+        data = {
             'device_id': self.device_id,
             'type': self.device_type,
             'sample_time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             'value': self.get_data(),
-        })
+        }
+        self.publish(self.world_topic, data)
+
+    def publish(self, topic, data):
+        payload = json.dumps(data)
         print(f'Publising in topic {self.world_topic}: {payload}')
         self.mqtt_client.publish(
-            topic=self.world_topic,
-            qos=1,
+            topic=topic,
             payload=payload,
+            qos=mqtt.QoS.AT_LEAST_ONCE,
         )
 
     def get_data(self):
